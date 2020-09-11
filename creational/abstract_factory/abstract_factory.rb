95720db3947ff1ff930f95b0d3693f48b7627980
Nicolás Nisoria <nicolas.nisoria@gmail.com>
2020-09-11 13:23:29 -0300

    Add abstract_factory implementation in ruby

---
+++ creational/abstract_factory/abstract_factory.rb
@@ -0,0 +1,89 @@
+class AbstractFactory 
+    def create_product_a
+        raise NotImplementedError, "#{self.class} has not implemented method '#{__method__}'"
+    end
+
+    def create_product_b
+        raise NotImplementedError, "#{self.class} has not implemented method '#{__method__}'"
+    end
+end
+
+class ConcreteFactory1 < AbstractFactory
+    def create_product_a
+        ConcreteProductA1.new
+    end
+
+    def create_product_b
+        ConcreteProductB1.new
+    end
+end
+
+class ConcreteFactory2 < AbstractFactory
+    def create_product_a
+        ConcreteProductA2.new
+    end
+
+    def create_product_b
+        ConcreteProductB2.new
+    end
+end
+
+class AbstractProductA
+    def operation_a
+        raise NotImplementedError, "#{self.class} has not implemented method '#{__method__}'"
+    end
+end
+
+class ConcreteProductA1 < AbstractProductA
+    def operation_a
+        'The result of the product A1.'
+    end
+end
+
+class ConcreteProductA2 < AbstractProductA
+    def operation_b
+        'The result of the product A2.'
+    end
+end
+
+class AbstractProductB
+    def operation_b
+        raise NotImplementedError, "#{self.class} has not implemented method '#{__method__}'"
+    end
+
+    def another_operation_b(collaborator)
+        raise NotImplementedError, "#{self.class} has not implemented method '#{__method__}'"
+    end
+end
+
+class ConcreteProductB1 < AbstractProductB
+    def operation_b
+        'The result of the product B1.'
+    end
+
+    def another_operation_b(collaborator)
+        result = collaborator.operation_a
+        "The result of the B1 collaborating with the (#{result})"
+    end
+end
+
+class ConcreteProductB2 < AbstractProductB
+    def operation_b
+        'The result of the product B2.'
+    end
+
+    def another_operation_b(collaborator)
+        result = collaborator.operation_a
+        "The result of the B2 collaborating with the (#{result})"
+    end
+end
+
+def client_code(factory)
+    product_a = factory.create_product_a
+    product_b = factory.create_product_b
+  
+    puts product_b.operation_b.to_s
+    puts product_b.another_operation_b(product_a).to_s
+end
+
+client_code(ConcreteFactory1.new)
